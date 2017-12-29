e381f0d61741cde988dbc307310aecf702edac4f
Marinator123 <marius.w@bluewin.ch>
2017-12-29 13:59:49 +0100

    Hello Jest Test hinzugefügt

--- js_test/__tests__/HelloTest.js
+++ js_test/__tests__/HelloTest.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import {configure, shallow} from 'enzyme';
 import Hello from '../src/Hello.jsx'
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
-test('CheckBox from Hello should change text after click', () => {
+test('Testet die Hello Komponente mit Jest Snapshot', () => {
+    const component = renderer.create(
+        <Hello labelOn="On" labelOff="Off" />,
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    tree.props.onChange();
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('Testet die Hello Checkbox mit Enzyme', () => {
     const checkbox = shallow(<Hello labelOn="On" labelOff="Off" />);
     
     expect(checkbox.text()).toEqual('HelloOff');
     checkbox.find('input').simulate('change');
     expect(checkbox.text()).toEqual('HelloOn');
 });
+
