36d094dce435402aea33465423c53cc25e36fbd1
Marinator123 <marius.w@bluewin.ch>
2017-12-31 10:44:49 +0100

    Gamelogik ausgelagert

--- SudokuReact/src/components/Game.jsx
+++ SudokuReact/src/components/Game.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Board from './Board.jsx';
-import range from 'lodash';
+import GameLogic from '../GameLogic.js'
 /**
  * 
  */
@@ -8,91 +8,16 @@ class Game extends React.Component {
 
     constructor(props) {
         super(props);
+        console.log(this.props.percEmptyFields)
+        const gameLogic = new GameLogic();
         this.state = {
-            squares: this.instantiateSquares(81)
+            squares: gameLogic.instantiateSquares(81, this.props.percEmptyFields)
         }
     }
-
-    instantiateSquares(size) {
-        // todo: funktioniert nicht mehr mit emptyFields -> eventuell wäre besser wenn zuerst alle initialisiert werden
-        // und danach wieder einzelne "gelöscht"
-        const squares = Array(size).fill({value:'', readOnly:false}); // temp, danahc auf null setzen
-        const alreadyTested = Array(size).fill(null);
-        const percEmptyFields = 0.0; // outsourcen
-        var i = 0;
-        while (i < size) {
-            if (Math.random() > percEmptyFields) {
-                var number;
-                if ((number = this.findUniqueNumber(i, squares, alreadyTested[i])) === undefined) {
-                    alreadyTested[i] = [];
-                    squares[i].value = '';
-                    i--;
-                    continue;
-                }
-                else {
-                    if (alreadyTested[i] === null) {alreadyTested[i] = []};
-                    alreadyTested[i].push(number);
-                    squares[i] = {value: number, readOnly: true};
-                }
-            } else {
-                squares[i] = {value: '', readOnly: false};
-            }
-            i++;
-        }
-        return squares;
-    }
-
-    findUniqueNumber(id, squares, numbersAlreadyTested) {
-        const rowId = Math.floor(id / 9);
-        const columnId = id % 9;
-        const occupiedNumbers = this.getOccupiedNumbersForField(rowId, columnId, squares, numbersAlreadyTested);
-        if (occupiedNumbers.length < 9)
-        {
-            while (true) {
-                const randomNumber = Math.floor((Math.random() * 9) + 1)
-                if (occupiedNumbers.indexOf(randomNumber) === -1) {
-                    return randomNumber;
-                }
-            }
-        }
-    }
-
-    getOccupiedNumbersForField(rowId, columnId, squares, numbersAlreadyTested)  {
-        /** Generiert die Startnummer des Felds z.B. 43 -> 33 */
-        const id = rowId * 9 + columnId;
-        const startFieldId = id - id % 3 - rowId % 3 * 9;
-        var occupiedNumbers = this.getAllValuesForRow(rowId, squares).concat(
-            this.getAllValuesForColumn(columnId, squares)).concat(this.getAllValuesForField(startFieldId, squares)).concat(
-                numbersAlreadyTested)
-        return occupiedNumbers.filter((x, i, a) => a.indexOf(x) == i); // find unique numbers
-    }
-
-    getAllValuesForRow(rowId, squares) {
-        const rowSize = squares.length / 9;
-        const rowSquares = squares.slice(rowId * rowSize, rowId * rowSize + rowSize);
-        return this.extractSquareValues(rowSquares);
-    }
-
-    getAllValuesForField(startId, squares) {
-        const fieldSquares = squares.slice(startId, startId + 3).concat(
-            squares.slice(startId + 9, startId + 12).concat(squares.slice(startId + 18, startId + 21)));
-        return this.extractSquareValues(fieldSquares);
-    }
-
-    getAllValuesForColumn(columnId, squares) {
-        const columnSquares = Array(9).fill(null).map((x,i) => {
-            return (squares[columnId + i*9])
-        });
-        return this.extractSquareValues(columnSquares);
-    }
-
-    extractSquareValues(squareValues) {
-        return squareValues.filter(a => a !== null).map(a => a.value).filter(val => val !== '');
-    }
-
+    
     setValues(id, value) {
         const squares = this.state.squares.slice();
-        // todo check ob squares legit sind
+        // evtl. todo check ob squares legit sind
         squares[id].value = value;
         this.setState({squares: squares})
     }
--- SudokuReact/src/main.js
+++ SudokuReact/src/main.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import GameLogic from  './GameLogic.js';
 import Game from  './components/Game.jsx';
 import Board from './components/Board.jsx';
 import Square from './components/Square.jsx';
 
-window.renderApp = function(idGame){
-    ReactDOM.render(<Game/>, document.getElementById(idGame));
+window.renderApp = function(idGame, percEmptyFields){
+    ReactDOM.render(<Game
+        percEmptyFields={percEmptyFields}
+    />, document.getElementById(idGame));
 };
 
