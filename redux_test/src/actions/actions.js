ac2d77f120e776f3bacc4a1ffa2c341c56e0c0f3
Marinator123 <marius.w@bluewin.ch>
2018-01-05 17:06:56 +0100

    Redux Tutorial hinzugefügt

---
+++ redux_test/src/actions/actions.js
@@ -0,0 +1,38 @@
+// Actions beschreiben, dass etwas was passiert ist
+// nicht wie die Applikation darauf reagieren soll
+
+/*
+ * action types
+ */
+
+export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+/*
+ * other constants
+ */
+// keine ahnung wo die benutzt werden
+export const VisibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+/*
+ * action creators
+ */
+// neuen Task hinzufügen
+export function addTodo(text) {
+  return { type: ADD_TODO, text }
+}
+
+// ein task als "komplett" abhaken
+export function toggleTodo(index) {
+  return { type: TOGGLE_TODO, index }
+}
+
+// Wechsel der zurzeit sichtbaren Tasks
+export function setVisibilityFilter(filter) {
+  return { type: SET_VISIBILITY_FILTER, filter }
+}
